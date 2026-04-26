bd2c010a20d5f7ae278c3db3128dec1c77d285f7
TwistyDog <leonardox445@gmail.com>
2026-04-26 14:12:58 -0300

    menu principal funcional

---
+++ Assets/Script/SCRIPT MENU/MainMenu.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] string nomeCenaJogo = "Area de Rua";
+    public string nomeOpcoes = "OptionScene";
+    
+    public void NovoJogo()
+    {
+        SceneManager.LoadScene(nomeCenaJogo);
+    }
+
+    public void Opcoes()
+    {
+        SceneManager.LoadScene(nomeOpcoes);
+    }
+
+    public void Sair()
+    {
+        Debug.Log("Saindo do Jogo");
+        Application.Quit();
+    }
+}
