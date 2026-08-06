e35acb1a47c9035ea5939f97c3bd96f0d95493ef
Leonardo Barros <leonardox445@gmail.com>
2026-08-06 20:15:53 -0300

    cursor funcionando.

--- Assets/Script/SCRIPT MENU/CharacterSelect.cs
+++ Assets/Script/SCRIPT MENU/CharacterSelect.cs
@@ -21,28 +21,104 @@ public class CharacterSelect : MonoBehaviour
     [SerializeField] private float cursorMoveSpeed = 0.15f;
 
         private int currentCharacters = 0;
+
+    private bool canSelect = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     private void OnEnable()
     {
         currentCharacters = 0;
+        canSelect = false;
 
         if (selectionCursor == null || characterPosition.Length == 0)
             return;
 
         selectionCursor.gameObject.SetActive(true);
 
-        selectionCursor.anchoredPosition =
-            characterPosition[currentCharacters].anchoredPosition;
+        selectionCursor.DOKill();
+
+        selectionCursor.position =
+            characterPosition[0].position;
+
+        Invoke(nameof(EnableSelection), 0.5f);
+    }
+
+    private void EnableSelection()
+    {
+        canSelect = true;
+    }
+
+    private void Update()
+    {
+        if (!canSelect)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            NextCharacter();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            PreviousCharacter();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            ConfirmCharacter();
+        }
+    }
+
+    private void NextCharacter()
+    {
+        currentCharacters++;
+
+        if (currentCharacters >= characterPosition.Length)
+            currentCharacters = 0;
+
+        MoveCursor();
+    }
+
+    private void PreviousCharacter()
+    {
+        currentCharacters--;
+
+        if (currentCharacters < 0)
+            currentCharacters = characterPosition.Length -1;
+
+        MoveCursor();
+    }
+
+    private void ConfirmCharacter()
+    {
+        SelectedCharacter = currentCharacters;
+
+        Debug.Log("Personagem Selecionado" + SelectedCharacter);
+
+        canSelect = false;
+
+        if(loadingManager != null)
+        {
+            loadingManager.StartLoading("Area de Rua");
+        }
+
+
     }
     public void SelectCharacter(int id)
     {
+        if (!canSelect)
+            return;
+
+        if (id < 0 || id >= characterPosition.Length)
+            return;
+
         currentCharacters = id;
-        SelectedCharacter = id;
 
         MoveCursor();
 
-        loadingManager.StartLoading("Area de Rua");
+        SelectedCharacter = id;
+
+        ConfirmCharacter();
     }
 
     private void MoveCursor()
@@ -54,9 +130,11 @@ public class CharacterSelect : MonoBehaviour
             currentCharacters >= characterPosition.Length)
             return;
 
+        selectionCursor.DOKill();
+
         selectionCursor
-            .DOAnchorPos(
-                characterPosition[currentCharacters].anchoredPosition,
+            .DOMove(
+                characterPosition[currentCharacters].position,
                 cursorMoveSpeed
             )
             .SetEase(Ease.OutQuad);
