19839ec2b12b73d42d32d1c9fc879381cebca942
Leonardo Barros <leonardox445@gmail.com>
2026-09-02 20:43:44 -0300

    quase completo

--- Assets/Script/SCRIPT MENU/MainMenu.cs
+++ Assets/Script/SCRIPT MENU/MainMenu.cs
@@ -10,9 +10,13 @@ public class MainMenu : MonoBehaviour
 
     [Header("Paineis")]
     [SerializeField] GameObject _mainMenuPainel;
+    [SerializeField] GameObject _gameModePanel;
     [SerializeField] GameObject _characterSelectPanel;
 
     [SerializeField] private Button[] menuButtons;
+
+    [Header("Botoes de Modo")]
+    [SerializeField] private Button[] _gameModeButton;
 
     [Header("Visual Selecao")]
     [SerializeField] Image _backgroundImage;
@@ -21,8 +25,21 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private Sprite OptionsSprite;
     [SerializeField] private Sprite quitSprite;
 
+    [Header("Visual Game Mode")]
+    [SerializeField] private Image _gameModeBackGround;
+
+    [SerializeField] private Sprite _arcadeSprite;
+    [SerializeField] private Sprite _onlineSprite;
+    [SerializeField] private Sprite _treinamentoSprite;
+    [SerializeField] private Sprite _voltarSprite;
+
+
 
     private int currentIndex = 0;
+
+    private int gameModeIndex = 0;
+
+    private GameMode _selectedMode;
 
     private void Start()
     {
@@ -39,29 +56,67 @@ public class MainMenu : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (_mainMenuPainel.activeSelf)
         {
-            currentIndex++;
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                currentIndex++;
 
-            if (currentIndex >= menuButtons.Length)
-                currentIndex = 0;
+                if (currentIndex >= menuButtons.Length)
+                    currentIndex = 0;
 
-            UptadeSelection();
+                UptadeSelection();
+            }
+
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                currentIndex--;
+
+                if (currentIndex < 0)
+                    currentIndex = menuButtons.Length - 1;
+
+                UptadeSelection();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                ExecuteSelected();
+                return; // <<< IMPORTANTE
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+
+        // ==========================================
+        // SELEÇÃO DE MODO
+        // ==========================================
+
+        if (_gameModePanel.activeSelf)
         {
-            currentIndex--;
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                gameModeIndex++;
 
-            if (currentIndex < 0)
-                currentIndex = menuButtons.Length - 1;
+                if (gameModeIndex >= _gameModeButton.Length)
+                    gameModeIndex = 0;
 
-            UptadeSelection();
-        }
+                UptadeGameModeSelection();
+            }
 
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            ExecuteSelected();
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                gameModeIndex--;
+
+                if (gameModeIndex < 0)
+                    gameModeIndex = _gameModeButton.Length - 1;
+
+                UptadeGameModeSelection();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SelecionarModo(gameModeIndex);
+                return;
+            }
         }
     }
 
@@ -95,9 +150,11 @@ public class MainMenu : MonoBehaviour
             case 0:
                 nextSprite = playSprite;
                 break;
+
             case 1:
                 nextSprite = OptionsSprite;
                 break;
+
             case 2:
                 nextSprite = quitSprite;
                 break;
@@ -112,7 +169,7 @@ public class MainMenu : MonoBehaviour
             _backgroundImage.sprite = nextSprite;
 
             _backgroundImage.rectTransform.localScale =
-            Vector3.one * 1.15f;
+                Vector3.one * 1.15f;
         });
 
         seq.Append(_backgroundImage.DOFade(1f, 0.25f));
@@ -138,18 +195,77 @@ public class MainMenu : MonoBehaviour
         }
     }
 
+    private void ChangeBackGroundGameMode()
+    {
+        Sprite nextSprite = null;
+
+        switch (gameModeIndex)
+        {
+            case 0:
+                nextSprite = _arcadeSprite;
+                break;
+
+            case 1:
+                nextSprite = _onlineSprite;
+                break;
+
+            case 2:
+                nextSprite = _treinamentoSprite;
+                break;
+
+            case 3:
+                nextSprite = _voltarSprite;
+                break;
+        }
+
+        Sequence seq = DOTween.Sequence();
+
+        seq.AppendCallback(() =>
+        {
+            _gameModeBackGround.sprite = nextSprite;
+
+            _gameModeBackGround.rectTransform.localScale =
+               Vector3.one * 1.15f;
+        });
+
+        seq.Append(
+            _gameModeBackGround
+            .DOFade(1f, 0.25f));
+
+
+        _gameModeBackGround.rectTransform
+            .DOScale(1f, 0.4f)
+            .SetEase(Ease.OutQuad);
+    }
+
 
 
     public void NovoJogo()
     {
         _mainMenuPainel.SetActive(false);
-        _characterSelectPanel.SetActive(true);
+        _gameModePanel.SetActive(true);
+        _characterSelectPanel.SetActive(false);
+
+        gameModeIndex = 0;
+        UptadeGameModeSelection();
     }
 
     public void VoltarMenu()
     {
         _characterSelectPanel.SetActive(false);
+        _gameModePanel.SetActive(false);
         _mainMenuPainel.SetActive(true);
+    }
+
+    public void VoltarParaGameMode()
+    {
+        _characterSelectPanel.SetActive(false);
+        _gameModePanel.SetActive(true);
+        _mainMenuPainel .SetActive(false);
+
+        gameModeIndex = (int)_selectedMode;
+
+        UptadeGameModeSelection();
     }
 
     public void Opcoes()
@@ -235,6 +351,73 @@ public class MainMenu : MonoBehaviour
             });
         });
 
+    }
+
+    private void UptadeGameModeSelection()
+    {
+        for(int i = 0; i < _gameModeButton.Length; i++)
+        {
+            if(i == gameModeIndex)
+            {
+                _gameModeButton[i].transform
+                    .DOScale(1.15f, 0.2f)
+                    .SetEase(Ease.OutQuad);
+            }
+            else
+            {
+                _gameModeButton[i].transform
+                    .DOScale(1f, 0.2f)
+                    .SetEase(Ease.OutQuad); 
+            }
+        }
+
+        ChangeBackGroundGameMode();
+    }
+
+    public void SelecionarModo(int modo)
+    {
+        _selectedMode = (GameMode)modo;
+
+        Debug.Log("Opção selecionada: " + _selectedMode);
+
+        switch (_selectedMode)
+        {
+            case GameMode.Arcade:
+                AbrirCharacterSelect();
+                break;
+
+            case GameMode.Online:
+                AbrirCharacterSelect();
+                break;
+
+            case GameMode.Treinamento:
+                AbrirCharacterSelect();
+                break;
+
+            case GameMode.Voltar:
+                VoltarMenu();
+                break;
+        }
+    }
+
+    public void AbrirCharacterSelect()
+    {
+        _mainMenuPainel.SetActive(false);
+        _gameModePanel.SetActive(false);
+        _characterSelectPanel.SetActive(true);
+
+        Debug.Log(
+        "Abrindo Character Select. Modo: " +
+        _selectedMode
+    );
+    }
+
+    public enum GameMode
+    {
+        Arcade,
+        Online,
+        Treinamento,
+        Voltar
     }
 
 }
